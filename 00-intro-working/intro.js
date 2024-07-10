a0978e17a5719bb3f0e0d6729f7ad7cf068ff30f
Robert Oleynick <rpoleynick@wpi.edu>
2024-07-10 14:48:10 -0400

    Add JavaScript and button function

--- 00-intro-working/intro.js
+++ 00-intro-working/intro.js
@@ -3,3 +3,18 @@
 //  1. Give every variable/const smallest scope
 //  2. Grab HTML elements by tag and store in const
 //  3. Put all other code inside functions
+
+const colorField = document.getElementById("color-field");
+const fontSizeField = document.getElementById("font-size-field");
+const clickButton = document.getElementById("click-button");
+const text = document.getElementById("text");
+const body = document.body;
+// let (or var) to declare variables
+
+function buttonPress() {
+    text.innerHTML = "Wow! You pressed the button!";
+
+    body.style.backgroundColor = colorField.value;
+    text.style.fontSize = `${fontSizeField.value}px`; // NOTE: the [`] key is above the [Tab] key
+    let fail = '${fontSizeField.value}px'; // Apostrophe will not work
+}
