c1a6b1b5f36aa4d2bdd07577ea1c2c5f9abfdffe
Robert Oleynick <rpoleynick@wpi.edu>
2024-07-10 15:07:52 -0400

    Working interactive site

--- 00-intro-working/intro.js
+++ 00-intro-working/intro.js
@@ -8,6 +8,7 @@ const colorField = document.getElementById("color-field");
 const fontSizeField = document.getElementById("font-size-field");
 const clickButton = document.getElementById("click-button");
 const text = document.getElementById("text");
+const checkbox = document.getElementById("checkbox");
 const body = document.body;
 // let (or var) to declare variables
 
@@ -17,4 +18,15 @@ function buttonPress() {
     body.style.backgroundColor = colorField.value;
     text.style.fontSize = `${fontSizeField.value}px`; // NOTE: the [`] key is above the [Tab] key
     let fail = '${fontSizeField.value}px'; // Apostrophe will not work
+}
+
+function checkboxChange() {
+    let checked = checkbox.checked;
+    console.log(`The checkbox has this state: ${checked}`);
+
+    if (checked) {
+        body.style.outlineStyle = "solid";
+    } else {
+        body.style.outlineStyle = "none";
+    }
 }
