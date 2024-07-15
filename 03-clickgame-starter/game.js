5bd179463fcfbf7b707740379c5e3b37be3e6c50
Rob-O-Code <40807772+Rob-O-Code@users.noreply.github.com>
2024-07-15 13:52:21 -0400

    Add starter files for clickgame

---
+++ 03-clickgame-starter/game.js
@@ -0,0 +1,42 @@
+function buy(store) {
+    
+}
+
+function harvest(widget) {
+    // Only run if currently not harvesting
+
+    // Set harvesting flag
+
+    // If manual, collect points now
+    if (widget.getAttribute("auto") != 'true') {
+        changeScore(widget.getAttribute("reap"));
+        showPoint(widget);
+    }
+
+    setTimeout(() => {
+        // Remove the harvesting flag
+
+        // If automatic, collect points
+        if (widget.getAttribute("auto") == 'true') {
+
+        }
+    }, 1000);
+}
+
+function changeScore(amount) {
+    score.innerHTML = score.innerHTML;
+
+    // Update the stores to block buying expensive boxes
+}
+
+function showPoint(widget) {
+    let number = document.createElement("span");
+    number.className = "point";
+    number.innerHTML = "+" + widget.getAttribute("reap");
+    number.style.left = "50%";
+    number.style.top = "50%";
+    number.onanimationend = () => {
+        widget.removeChild(number);
+    }
+    widget.appendChild(number);
+}
